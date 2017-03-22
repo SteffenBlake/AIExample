1626150762974b410451f50403de39514bdc07d2
Steffen Blake <Steffen.Blake.82@pleasantsolutions.com>
2017-03-22 16:52:20 -0600

    Added both types of AI

--- SimpleAI/Program.cs
+++ SimpleAI/Program.cs
@@ -11,32 +11,76 @@ namespace SimpleAI
     {
         static int ENTERKEY = 13;
 
+        private static double _Errorthresh = 0.01;
+
         static void Main(string[] args)
+        {
+            LogicGateAI();
+
+            Console.WriteLine("Press any key to end the program...");
+            Console.ReadKey();
+        }
+
+        static void MoverAI()
         {
             var Service = new DeepThink(1, 1, 4, 4);
             var x = 10;
             var y = 10;
 
-
             while (true)
             {
                 DrawScreen(0, 0);
-                var keyDown = (int)Console.ReadKey().Key;
+                var keyDown = (int) Console.ReadKey().Key;
                 if (keyDown == ENTERKEY)
                 {
                     Service.Punish();
                 }
                 else
                 {
-                    var outputs = Service.Run(new[] {keyDown / 220D }).Select(v => Math.Round(v)).ToArray();
+                    var outputs = Service.Run(new[] {keyDown / 220D}).Select(v => Math.Round(v)).ToArray();
                     if (outputs[0] != 0) x -= 1;
                     if (outputs[1] != 0) y -= 1;
                     if (outputs[2] != 0) x += 1;
                     if (outputs[3] != 0) y += 1;
                 }
+            }
+        }
 
+        static void LogicGateAI()
+        {
+            var service = new DeepThink(1, 2, 4, 1);
+
+            var TrainingData = new Dictionary<double[], double[]>();
+
+            TrainingData.Add(new[] { 0D, 0D }, new[] { 0D });
+            TrainingData.Add(new[] { 0D, 1D }, new[] { 1D });
+            TrainingData.Add(new[] { 1D, 0D }, new[] { 0D });
+            TrainingData.Add(new[] { 1D, 1D }, new[] { 1D });
+
+            var currentError = 1D;
+            for (int n = 0; currentError > _Errorthresh; n++)
+            {
+                Console.SetCursorPosition(0, 0);
+                Console.WriteLine("Training - Sets: {0} - Error threshold: {1:P}", n, _Errorthresh);
+                var errorList = new List<double>();
+                foreach (KeyValuePair<double[], double[]> trainer in TrainingData)
+                {
+                    service.Train(trainer.Key, trainer.Value);
+                    errorList.Add(service.LastError);
+                }
+                currentError = Math.Sqrt(errorList.Sum(e => e * e));
             }
 
+            Console.WriteLine("Error: {0:P}", currentError);
+
+            foreach (KeyValuePair<double[], double[]> Trainer in TrainingData)
+            {
+                Console.WriteLine("Inputs: {0}", string.Join("-", Trainer.Key));
+                Console.WriteLine("Expected Out: {0}", string.Join("-", Trainer.Value));
+
+                var outputs = service.Run(Trainer.Key);
+                Console.WriteLine("Actual Out: {0}\n", string.Join("-", outputs));
+            }
         }
 
         //Draws the screen. X must be 1-30, y 1-15
