58927a1e235ed342004ab8235141db052db6739a
Steffen Blake <Steffen.Blake.82@pleasantsolutions.com>
2017-03-30 19:07:52 -0600

    Simple AI : Added Nerves

---
+++ SimpleAI/Nerve Models/Link.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleAI.Models
+{
+    public interface ILink
+    {
+        IList<INerve> Nerves { get; set; }
+
+        double Weight { get; set; }
+
+        double Threshould { get; set; }
+
+        double Value { get;}
+
+        bool CanFire { get; }
+    }
+
+    public class Link : ILink
+    {
+        public IList<INerve> Nerves { get; set; }
+
+        public double Weight { get; set; }
+
+        public double Threshould { get; set; }
+
+        public bool CanFire => Nerves.Any(n => !n.Fired) && Nerves.Any(n => n.Armed);
+
+        public double Value => CanFire && Nerves.Single(n => n.Armed).Output * Weight >= Threshould ? Nerves.Single(n => n.Armed).Output * Weight : 0D;
+
+        public Link()
+        {
+            Weight = 1D;
+            Threshould = 0D;
+        }
+
+    }
+}
---
+++ SimpleAI/Nerve Models/Nerve.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using SimpleAI.Models;
+
+namespace SimpleAI.Models
+{
+    public interface INerve
+    {
+        IList<ILink> Links { get; set; }
+
+        bool Armed { get; set; }
+
+        bool Fired { get; set; }
+
+        double Output { get; }
+    }
+}
+
+public class Nerve : INerve
+{
+    public IList<ILink> Links { get; set; }
+
+    public bool Armed { get; set; }
+
+    public bool Fired { get; set; }
+
+    public double Output => 1D;
+
+    public Nerve()
+    {
+        Links = new List<ILink>();
+        Fired = false;
+    }
+}
